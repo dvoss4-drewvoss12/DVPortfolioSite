33d6a1e2ac2eac362e2c816178b62ba498cd7c62
Drew Voss <drewvoss12@gmail.com>
2026-05-31 13:40:53 -0500

    Added site parameter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-
+  site: "https://drewvoss.dev",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
